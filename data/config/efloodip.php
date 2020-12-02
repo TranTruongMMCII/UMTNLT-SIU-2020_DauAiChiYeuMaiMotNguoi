1cb6ddb9ba07c97f2e83a2cf00f2af419968dd3a
TranTruongMMCII <truongtlt147@gmail.com>
2020-12-02 09:42:16 +0700

    First commit

---
+++ data/config/efloodip.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author VINADES.,JSC <contact@vinades.vn>
+ * @Copyright (C) 2020 VINADES.,JSC. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Wed, 02 Dec 2020 02:15:33 GMT
+ */
+
+if (!defined('NV_MAINFILE')) {
+    die('Stop!!!');
+}
+
+$array_except_flood_site = [];
+$array_except_flood_site['::1'] = ['ip6' => 1, 'mask' => "::1/0", 'begintime' => 1606875333, 'endtime' => 0];
+
+$array_except_flood_admin = [];
